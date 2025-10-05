bd7db97ce04bc57904da98588aaba0607c65c111
MADANI1972 <madanimhamed1972@gmail.com>
2025-10-05 22:15:29 +0200

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function AdminPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Rediriger automatiquement vers login ou dashboard
+    router.push('/admin/login');
+  }, [router]);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Redirection...</p>
+    </div>
+  );
+}
